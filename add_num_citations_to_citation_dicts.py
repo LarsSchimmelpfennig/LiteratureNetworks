f916993f83f359157029ef11c781d14ec68d284f
LarsSchimmelpfennig <91089724+LarsSchimmelpfennig@users.noreply.github.com>
2022-10-24 18:44:47 -0500

    Add files via upload

---
+++ add_num_citations_to_citation_dicts.py
@@ -0,0 +1,119 @@
+import orjson as json
+import os
+import time
+import re
+import progressbar
+
+def add_num_citations():
+	for year in range(1995, 2022):
+		if '{}_num_citations_yearsv2.json'.format(year) in os.listdir('D:\citation networks'):
+			continue
+		t1 = time.time()
+		#this will give me a list of the files that fall under year
+		files = os.listdir('D:\citation networks')
+		#2021_citations_01.json
+		l_names = []
+		for file in files:
+			name = re.findall(r'[0-9]{4}_citations_[0-9]{2}.json',file)
+			if name != [] and name[0][:4] == str(year):
+				l_names.append(name[0])
+		print(l_names)
+		print(year)
+		#here I have a list of starting files of one year - I want to create a lookup dict from all of these
+		#file = '{}_citations_years.json'.format(year)
+		years_d = {}
+		for file in l_names:
+			f = open(os.path.join('D:\citation networks',file))
+			d = json.loads(f.read())
+			#this is the starting dict that will contain parents with their num citations but not for children
+			#replace '1998, id' with '1998, 2, id' to add num citations
+			for parent in d.keys():
+				num_citations = len(d[parent])
+				updated_parent = parent.replace(',', ', '+str(num_citations)+',')
+				#for each child I want to add them to respective year of new dict including dict based on parent
+				for child in d[parent]:
+					child_year = child.split(',')[0]
+					if int(child_year) < 1900 or int(child_year) > 2021:
+						continue
+					edge = updated_parent+'->'+child
+					if child_year not in years_d.keys():
+						years_d[child_year] = [edge]
+					else:
+						years_d[child_year].append(edge)
+		print('finished creating starting dictionary of years containing edge lists')
+		del d
+		del f
+		final_edge_list = []
+		l_years = list(years_d.keys())
+		l_years.sort()
+		#years_d.keys().sort()
+		#after going through the entire starting year file and creating a complete years_d I will now go through each year file
+		#this program will take a very long time as I need to open so many future year files
+		timer = time.time()
+		for curr_year in l_years:
+			start_time = time.time()
+			edge_list = years_d[curr_year]
+			l_names = []
+			for file in os.listdir('D:\citation networks'):
+				name = re.findall(r'[0-9]{4}_citations_[0-9]{2}.json',file)
+				if name != [] and name[0][:4] == str(curr_year):
+					l_names.append(name[0])
+			#bar = progressbar.ProgressBar(maxval=20, widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+			#bar.start()
+			#with this list of files I want to go through each file then iterate through all keys - if I find I remove and add to final edge list
+			for idx, file in enumerate(l_names):
+				#bar.update(idx+1)
+				#print(file)
+				t2 = time.time()
+				f = open(os.path.join('D:\citation networks',file))
+				d = json.loads(f.read())
+				#print(file, "loaded in: ", (time.time()-t2)/60, ' mins')
+				d_keys_set = set(d.keys())
+				
+				#found will be based on the previous not_found after each iteration
+				if idx == 0:
+					#this is the first iteration/file
+					found = [item for item in edge_list if item.split('->')[1] in d_keys_set]
+					not_found = [item for item in edge_list if item.split('->')[1] not in d_keys_set]
+					#print('first file: ',len(found), len(not_found))
+				else:
+					#after the first iteration found will be based on prev 
+					found = [item for item in not_found if item.split('->')[1] in d_keys_set]
+					not_found = [item for item in not_found if item.split('->')[1] not in d_keys_set]
+					#print('file ',idx+1, ': ',len(found),' ', len(not_found))
+				#print(file, "loaded in: ", (time.time()-t2)/60, ' mins')
+				#for edge in edge_list[::-1]:
+				#t2 = time.time()
+				for edge in found:
+					parent,child = edge.split('->')
+					if child in d_keys_set:
+						child_num_citations = len(d[child])
+						updated_child = child.replace(',', ', '+str(child_num_citations)+',')
+						final_edge_list.append(parent+'->'+updated_child)
+			print('len of edge list for year: ',curr_year, " ", len(edge_list), 'time: ', (time.time()-start_time)/60, ' mins')
+			#bar.finish()
+				#print('time to look through' ,file,  ' with edge_list: ', (time.time()-t2), ' secs')
+				
+		del d
+		to_return_d = {}
+		print('finished creating updated edge list for given year in ', (time.time()-timer)/60, ' mins')
+
+		#now I should have a completed edge list and can reform the large dict and write it to the current year file
+		for edge in final_edge_list:
+			parent, child = edge.split('->')
+			if parent in to_return_d.keys():
+				to_return_d[parent].append(child)
+			else:
+				to_return_d[parent] = [child]
+		del final_edge_list
+
+		curr_file_name = '{}_num_citations_yearsv2.json'.format(year)
+		with open(os.path.join('D:\citation networks',curr_file_name), 'wb') as outfile:#, encoding='utf-8') as outfile:
+			outfile.write(json.dumps(to_return_d))
+			#json.dump(to_return_d, outfile, ensure_ascii=False, indent=4)
+		del to_return_d
+		print('completed: ',(time.time()-t1)/60, ' mins')
+		print('-------------------------------------------------------------------------------------')
+		
+
+add_num_citations()
---
+++ converting_reference_files_to_citation_dict.py
@@ -0,0 +1,132 @@
+import json
+import os
+import sys
+sys.path.append(r"C:\Users\1Lars\OneDrive\Desktop\Visual Studio Code\projects\wos_explorer_master\wos_explorer")
+from article_collection import ArticleCollection
+import time
+import csv
+from dask import dataframe as dd
+import pandas as pd
+
+#create a year citation file
+def add_curr_d(parent, curr_d, idx, l_citations_children):
+	if parent in curr_d.keys() and l_citations_children[idx] not in curr_d[parent]:
+		curr_d[parent].append(l_citations_children[idx])
+	#else we want to create a list with this child
+	else:
+		curr_d[parent] = [l_citations_children[idx]]
+
+def citation_year_dicts(target_year):
+	t1 = time.time()
+	#first we write an edge list for each core file to a temp csv
+	files = os.listdir('D:\citation networks')
+	with open(os.path.join('D:\citation networks','{}_temp_edge_list.csv'.format(target_year)),'w') as temp_csv:
+		writer=csv.writer(temp_csv, delimiter=',',lineterminator='\n')
+		#header
+		writer.writerow(['parent', 'cited_by'])
+		for file in files:
+			file_year = file[:4]
+			if file[5:9] == 'CORE' and file_year == target_year:
+				print(file)
+				for article in ArticleCollection(os.path.join('D:\citation networks',file)):
+					#curr_year is the same as target_year?
+					curr_year = article['pub_year']
+					curr_id = article['id']
+					l_refs = list(article.reference_list())#.ids())
+					for year, refs in l_refs:
+						if year == '' or year == None or len(year) > 4 or int(year) < 1500:
+							continue
+						#these are sorted so I can stop here
+						if int(year) > int(target_year):
+							break
+						#each row is a touple of a year - followed by the corresponing citation id
+						refs = list(refs)
+						#print(year,refs)
+						for ref in refs:
+							if ref == '' or ref == None:
+								continue
+							#print(ref)
+							citation_edge = [year + ', '+ ref, curr_year + ', ' + curr_id]
+							writer.writerow(citation_edge)
+				#print('deleting core file')
+				#os.remove(os.path.join('D:\citation networks', file))	
+
+	print('done writing {} temp edge list'.format(target_year))
+	#need to sort edge list here
+	start = time.time()
+	#print('sorting edge list')
+	dask_df = dd.read_csv(os.path.join('D:\citation networks','{}_temp_edge_list.csv'.format(target_year)))
+	dask_df =  dask_df.set_index(dask_df.columns[0]).persist()
+	#print('writing to csv')
+	dask_df.to_csv(os.path.join('D:\citation networks','sorted_{}_temp_edge_list.csv'.format(target_year)), single_file = True)
+	del dask_df
+	print('completed sorting: ', (time.time()-start)/60, 'mins')
+	os.remove(os.path.join('D:\citation networks','{}_temp_edge_list.csv'.format(target_year)))
+	
+	curr_d = {}
+	num_chunks = len(list(pd.read_csv('D:\citation networks/sorted_{}_temp_edge_list.csv'.format(target_year), chunksize=10000)))
+	increment = int(num_chunks / 4)
+	print('number of chunks to scan: ',num_chunks)
+	for num_chunk, chunk in enumerate(pd.read_csv('D:\citation networks/sorted_{}_temp_edge_list.csv'.format(target_year), chunksize=10000)):
+		if increment > 0 and (num_chunk+1) % increment == 0:
+			print('progress: ',((num_chunk+1) / increment) * 25,'%')
+		l_citations_parents = chunk["parent"].values.tolist()
+		l_citations_children = chunk["cited_by"].values.tolist()
+		year = l_citations_parents[0].split(',')[0]
+		curr_file_year = year
+		curr_file_name = '{}_citations_years.json'.format(curr_file_year)
+
+		if curr_d == {} and curr_file_name in os.listdir('D:\citation networks'):
+			#print('at start, loading curr_d from prev file')
+			f = open(os.path.join('D:\citation networks', curr_file_name))
+			curr_d = json.load(f)
+			f.close()
+
+		#if this file year doesnt exist yet it will be created later and curr_d is {}
+		for idx, parent in enumerate(l_citations_parents):
+			year = parent[:4]
+
+			#if we have reached the end of the last chunk I want to write this to a file
+			if num_chunk+1 == num_chunks and idx+1 == len(l_citations_parents):
+				add_curr_d(parent, curr_d, idx, l_citations_children)
+				with open(os.path.join('D:\citation networks',curr_file_name), 'w', encoding='utf-8') as outfile:
+					json.dump(curr_d, outfile, ensure_ascii=False, indent=4)
+				#print('reached end of sorted edge list')
+				break
+
+			#if the next id year is a repeat
+			if year == curr_file_year: #and (len(curr_d.keys()) < max_len or l_citations_parents[idx + 1] == l_citations_parents[idx]):
+				add_curr_d(parent, curr_d, idx, l_citations_children)
+				continue
+
+			#if we are at a new year then we want to add this dict to a file
+			if year != curr_file_year:
+				with open(os.path.join('D:\citation networks',curr_file_name), 'w', encoding='utf-8') as outfile:
+					json.dump(curr_d, outfile, ensure_ascii=False, indent=4)
+				
+				curr_file_year = year
+				curr_file_name = os.path.join('{}_citations_years.json'.format(curr_file_year))
+
+				#start filling up new dict depending if already exists or not
+				if curr_file_name in os.listdir('D:\citation networks'):
+					f = open(os.path.join('D:\citation networks', curr_file_name))
+					curr_d = json.load(f)
+					f.close()
+				else:
+					curr_d = {}
+				add_curr_d(parent, curr_d, idx, l_citations_children)
+	print('completed, ', (time.time()-t1)/60, 'mins')
+	#print('deleting sorted temp edge list')
+	print('-------------------------------------------------------------------------------')
+	os.remove(os.path.join('D:\citation networks','sorted_{}_temp_edge_list.csv'.format(target_year)))
+
+#citation_year_dicts('1999')
+#whichever year is the highest citations_years file is the current year
+for year in range(1900, 2022):
+	files = os.listdir('D:\citation networks')
+	#example CORE file name: 1999_CORE_002.json
+	for file in files:
+		if file[:4] == str(year) and file[5:9] == 'CORE' and '{}_citations_years.json'.format(year) not in files:
+			print('starting year: ',year)
+			citation_year_dicts(str(year))
+			break
